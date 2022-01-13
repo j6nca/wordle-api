5c505062bd50b82acd3c2220f2efc47e0ebeba8b
jngbot <jng.cad@gmail.com>
2022-01-12 21:14:12 -0500

    added words and answers

--- app.py
+++ app.py
@@ -3,9 +3,33 @@ from flask import request
 from datetime import datetime as dt
 app = flask.Flask(__name__)
 
+def loadAns():
+    ansFile = open("answers", "r")
+    ans = ansFile.read()
+    ansFile.close()
+    ansList = ans.replace('"', "").split(",")
+    return ansList
+
+def loadWords():
+    wordsFile = open("answers", "r")
+    words = wordsFile.read()
+    wordsFile.close()
+    return words
+
+answers = loadAns()
+words = loadWords()
+
 @app.route('/', methods=['GET'])
-def home():
+def root():
+    return "hello world"
+
+
+@app.route('/today', methods=['GET'])
+def today():
     # arg = request.args['arg1']
     return "hello world"
 
+@app.route('/words', methods=['GET'])
+def words():
+    return words
 
