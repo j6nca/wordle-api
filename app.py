21300f805c4726d7cf4b5150945ca9da65f57ad9
jngbot <jng.cad@gmail.com>
2022-01-12 21:29:53 -0500

    days answers support

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request
-from datetime import datetime as dt
+import datetime
 app = flask.Flask(__name__)
 
 def loadAns():
@@ -14,10 +14,15 @@ def loadWords():
     wordsFile = open("answers", "r")
     words = wordsFile.read()
     wordsFile.close()
-    return words
+    wordsList = words.replace('"', "").split(",")
+    return wordsList
 
 answers = loadAns()
-valid_words = loadWords()
+validWords = loadWords()
+
+# print(answers.index('favor')) -> 207
+# start date = today - 207 days -> June 19, 2021
+startDate = datetime.datetime(2021, 6, 19)
 
 @app.route('/', methods=['GET'])
 def root():
@@ -26,9 +31,11 @@ def root():
 
 @app.route('/today', methods=['GET'])
 def today():
-    return "hello world"
+    currDate = datetime.datetime.now()
+    ansIndex = (currDate - startDate).days
+    return answers[ansIndex]
 
 @app.route('/words', methods=['GET'])
 def words():
-    return valid_words
+    return validWords
 
